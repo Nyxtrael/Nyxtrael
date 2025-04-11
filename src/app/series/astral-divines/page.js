6ff107ca4487122b72d8812accf0b47fc29acf86
Nyxtrael <nyxtrael@gmail.com>
2025-04-11 03:29:33 +0200

    lol

--- src/app/series/astral-divines/page.js
+++ src/app/series/astral-divines/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import galleryData from '../../data/galleryData'; 
+import galleryData from '../src/data/galleryData'; 
 import Image from 'next/image';
 import { useState } from 'react';
 
