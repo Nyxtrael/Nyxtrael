1c6f4e31c9b06c62256c91369c073381c7509a70
Nyxtrael <nyxtrael@gmail.com>
2025-04-29 22:12:44 +0200

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginNext from '@next/eslint-plugin-next';
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const config = {
+  plugins: {
+    react: eslintPluginReact,
+    '@next/next': eslintPluginNext,
+  },
+  rules: {
+  
+    'react/no-unescaped-entities': 'off',
 
-export default eslintConfig;
+    '@next/next/no-img-element': 'off',
+  },
+};
+
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 // next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // WAŻNE dla Netlify
+    unoptimized: true, // Netlify tego chce, nie kłóć się z nią
+  },
+  experimental: {
+    appDir: true, // wymagane dla Next.js 13+ app directory
   },
 };
 
