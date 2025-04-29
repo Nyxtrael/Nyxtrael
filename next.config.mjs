8986447defa16c24fbb8eaf8e7fdaea4b8826769
Nyxtrael <nyxtrael@gmail.com>
2025-04-29 22:26:53 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Netlify tego chce, nie kłóć się z nią
-  },
-  experimental: {
-    appDir: true, // wymagane dla Next.js 13+ app directory
+    unoptimized: true, // Good for Netlify
   },
 };
 
