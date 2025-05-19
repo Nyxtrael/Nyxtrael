c6dbc57dad704339880396ab6aa4c210b1d40e57
Nyxtrael <nyxtrael@gmail.com>
2025-05-20 00:06:27 +0200

    ASDAS

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
+// next.config.js
+const path = require('path');
 
 const nextConfig = {
-  
-  images: {
-    domains: ['localhost'], 
-  },
- 
   reactStrictMode: true,
-  
   trailingSlash: true,
+
+  images: {
+    domains: ['localhost'], // + res.cloudinary.com gdy używasz Cloudinary
+  },
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
